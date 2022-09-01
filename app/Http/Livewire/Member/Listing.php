401ff33bb9881c56dc82073af80e07390e333ad7
Shabeersha-Tom <102294384+Shabeersha-Tom@users.noreply.github.com>
2022-09-01 18:44:38 +0400

    EOD 01 09

--- app/Http/Livewire/Member/Listing.php
+++ app/Http/Livewire/Member/Listing.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire\Member;
 
+use App\Models\Location\Package;
 use App\Models\Member\Member;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -13,13 +14,25 @@ class Listing extends Component
 
     public $search = '';
 
-    public $location = '';
+    public $packages;
+
+    public $package = 2;
 
     public $currentStatus;
 
     public function updatingSearch()
     {
         $this->resetPage();
+    }
+
+    public function changePackage()
+    {
+        $this->render();
+    }
+
+    public function mount()
+    {
+        $this->packages = Package::all();
     }
 
     public function toggleStatus($id)
@@ -33,9 +46,11 @@ class Listing extends Component
     public function render()
     {
         return view('livewire.member.listing', [
-            'members' => Member::where('name', 'LIKE', '%' . $this->search . '%')
+            'members' => Member::where('package_id',  (int)$this->package)
+                ->where('name', 'LIKE', '%' . $this->search . '%')
                 ->orWhere('email', 'LIKE', '%' . $this->search . '%')
                 ->orWhere('phone', 'LIKE', '%' . $this->search . '%')
+
                 ->paginate(10),
         ]);
     }
